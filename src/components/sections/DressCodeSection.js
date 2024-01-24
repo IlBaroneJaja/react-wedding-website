5ee12199531907cc464606c0fa7a3dc4d32dfd8d
Alessandro Vassalli <alessandro.vassalli@toyotaconnected.eu>
2024-01-24 11:10:04 +0100

    Fix DressCode and GiftRegistries styling

--- src/components/sections/DressCodeSection.js
+++ src/components/sections/DressCodeSection.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import sharedStyles from "../../index.module.css";
+import styles from "./DressCodeSection.module.css";
 const DressCodeSection = () => {
     return (
-        <section id="dress-code" className={sharedStyles.otherSections}>
+        <section id="dress-code" className={`${sharedStyles.otherSections} ${styles.dressCodeBgImage}`}>
             <div className={sharedStyles["text-overlay"]}>
-                <p>Contenu pour la liste de mariage</p>
+                <p>Contenu pour le dress code</p>
                 <h1>Event Itineraries</h1>
             </div>
         </section>
--- src/components/sections/GiftRegistriesSection.js
+++ src/components/sections/GiftRegistriesSection.js
@@ -2,8 +2,8 @@ import React from 'react';
 import sharedStyles from "../../index.module.css";
 const GiftRegistriesSection = () => {
     return (
-        <section id="gift-registries" className="otherSections">
-            <div className="text-overlay">
+        <section id="gift-registries" className={sharedStyles.otherSections}>
+            <div className={sharedStyles["text-overlay"]}>
                 <p>Contenu pour les listes de mariage</p>
                 <h1>Event Itineraries</h1>
             </div>
